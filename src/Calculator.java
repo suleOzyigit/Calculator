2bdba658f914446e2d80ea747dca521276cc5a09
Sule <suleouslu@gmail.com>
2019-01-19 12:54:16 -0500

    new adding

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,4 +4,10 @@ public class Calculator {
 
         return num1+num2;
     }
+
+    public static double subtract(double num1, double num2){
+
+        double result=num1-num2;
+        return result;
+    }
 }
