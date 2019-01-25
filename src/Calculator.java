c08381d6551e7947c187a4e3a9e8de20bd768790
suleOzyigit <44989704+suleOzyigit@users.noreply.github.com>
2019-01-25 12:22:59 -0500

    changed something

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,9 @@
 public class Calculator {
 
     public static double add(double num1,double num2){
+        double sum=num1+num2;
 
-        return num1+num2;
+        return sum;
     }
 
     public static double subtract(double num1, double num2){
