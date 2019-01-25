a27be792a26d4b4c3fb59f48b9debf69587ac1ee
Sule <suleouslu@gmail.com>
2019-01-25 12:07:38 -0500

    division is added

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,4 +13,12 @@ public class Calculator {
     public static double multiple(double num1,double num2){
     return num1*num2;
     }
+    public static double division(double num1,double num2){
+
+        if(num2!=0){
+            return num1/num2;
+        }else{
+            return -1;
+        }
+    }
 }
